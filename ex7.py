e7cc8889f483ac04076d6cfb6eddcc35554bbc9a
vrubanov2009-prog <vrubanov2009@gmail.com>
2025-10-17 18:43:09 +0000

    it

---
+++ ex7.py
@@ -0,0 +1,13 @@
+def xnplus(h, a):
+    a = str(a)
+    v1 = (h + 1)/2 - 1
+
+    for i in range(h):
+        for j in range(h):
+            if i == j or i + j == h - 1 or i == v1 or j == v1:
+                print(a, end = " ")
+            else:
+                print(" ", end = " ")
+        print("")
+
+xnplus(11, "#")
